8bf14c95ff237d65d08b407af339a9631706011e
Yiqian Yang <yiqianya@usc.edu>
2024-01-10 19:59:38 -0800

    setup mongodb connection

--- server/index.js
+++ server/index.js
@@ -5,7 +5,17 @@ const cors = require("cors");
 app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 const { default: mongoose } = require("mongoose");
 require("dotenv").config();
-mongoose.connect(process.env.MONGODB_URL);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
