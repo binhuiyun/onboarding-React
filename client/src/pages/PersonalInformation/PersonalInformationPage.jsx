b28c7baa3343922cd835582b5ee14302f9c7ee54
Yiqian Yang <yiqianya@usc.edu>
2024-01-10 19:53:56 -0800

    set up backend library

---
+++ client/src/pages/PersonalInformation/PersonalInformationPage.jsx
@@ -0,0 +1,11 @@
+import { React, useEffect, useState } from "react";
+
+const PersonalInformationPage = () => {
+    return (
+        <div>
+            <h1>Personal Information Page</h1>
+        </div>
+    );
+};
+
+export default PersonalInformationPage;
---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+app.use(express.json());
+const cors = require("cors");
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
+mongoose.connect(process.env.MONGODB_URL);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(4000, () => console.log("Server running on port 4000"));
