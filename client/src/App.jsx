91a2dcb9f719b6efef2daedc705dc07068226c8a
Yiqian Yang <yiqianya@usc.edu>
2024-01-12 01:43:59 -0800

    remove tailwind provided css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/personal-information" element={<PersonalInformationPage />} />
     </Routes>
     </BrowserRouter>
   )
