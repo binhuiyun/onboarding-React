f603987fe045f5f86892a6250c0118655b068d60
baby bin <binhuiyun@gmail.com>
2024-01-12 14:09:07 -0800

    add tokenservice

---
+++ client/src/pages/HiringManagement/index.jsx
@@ -0,0 +1,8 @@
+import "React" from "react";
+
+export default function HiringManagement() {
+  return <div>  
+    <h1>Hiring Management</h1>
+    <button>Generate token and send email</button>
+  </div>;
+}
