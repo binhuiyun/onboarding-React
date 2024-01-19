2b6d638eaa4c83e3f201b0a671d2e3fbe8be75f8
LycheeCalpico <rachelqiu0428@gmail.com>
2024-01-18 16:09:08 -0800

    temp api change

--- server/handlers/visa.js
+++ server/handlers/visa.js
@@ -1,4 +1,5 @@
 const { visaModel } = require("../models/visaDocuments");
+const PersonalInformation = require("../models/personalInformation");
 const getAllVisa = async (req, res) => {
   try {
     const visaDoc = await visaModel.find();
@@ -31,6 +32,9 @@ const addToVisaDocumentation = async (req, res) => {
   try {
     const fileType = req.params.fileType;
     const visaDoc = await visaModel.findOne({ user: req.params.id });
+    if (!visaDoc) {
+      visaDoc = await visaModel.create({ user: req.params.id });
+    }
     visaDoc[fileType] = {
       fileName: req.file.originalname,
       fileDoc: req.file.buffer,
@@ -56,11 +60,74 @@ const downloadEmptyAndSample = async (req, res) => {
     res.status(500).json(err);
   }
 };
+// temp api for hr side table
 const getHrSideData = async (req, res) => {
   try {
-    const visaDoc = await visaModel.find();
-  } catch (err) {}
+    const arr = [];
+    for (const model of visaModel) {
+      const id = model.user;
+      const profileData = await PersonalInformation.findOne({ user: id });
+      const nextStep = "";
+      const action = "";
+      const docStatus = "IN PROGRESS";
+      if (model[optReceipt] && model[optReceipt].status === "pending") {
+        nextStep = "Waiting for HR to approve the OPT Receipt";
+        action = "need review";
+      } else if (model[optEAD] && model[optEAD].status === "pending") {
+        nextStep = "Waiting for HR to approve the OPT EAD";
+        action = "need review";
+      } else if (model[I983] && model[I983].status === "pending") {
+        nextStep = "Waiting for HR to approve and sign the I983";
+        action = "need review";
+      } else if (model[I20] && model[I20].status === "pending") {
+        nextStep = "Waiting for HR to approve the I20";
+        action = "need review";
+      } else if (model[optReceipt] === "approved" && !model[optEAD]) {
+        nextStep = "Please upload a copy of your OPT EAD";
+        action = "send notification";
+      } else if (model[optEAD] === "approved" && !model[I983]) {
+        nextStep = "Please download and fill out the I-983 form";
+        action = "send notification";
+      } else if (model[I983] === "approved" && !model[I20]) {
+        nextStep =
+          "Please send the I-983 along all necessary documents to your school and upload the new I-20";
+        action = "send notification";
+      } else if (model[I20] === "approved") {
+        nextStep = "All documents have been approved";
+        docStatus = "DONE";
+      }
+
+      if (profileData) {
+        const { name, employment } = profileData;
+        const data = {
+          firstName: name.firstName,
+          lastName: name.lastName,
+          preferredName: name.preferredName,
+          Work_Authorization_title: employment.visaTitle,
+          Work_Authorization_start_data: employment.startDate,
+          Work_Authorization_end_data: employment.endDate,
+          Work_Authorization_remaining:
+            employment.startDate.getTime() - employment.endDate.getTime(),
+          optReceipt: model.optReceipt,
+          optEAD: model.optEAD,
+          I983: model.I983,
+          I20: model.I20,
+          nextStep: nextStep, // The NEXT STEP of action
+          docStatus: docStatus, // finish all or not; if finished: DONE; if not: IN PROGRESS
+          action: action,
+        };
+
+        arr.push(data);
+      }
+    }
+
+    res.status(200).json(arr);
+  } catch (err) {
+    console.error(err); // Log the error for debugging purposes
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
+
 module.exports = {
   createVisaModel,
   getVisaById,
