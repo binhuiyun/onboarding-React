ee2eaf7dea1d58fd1d7565a995a643fd47bdfd6e
LycheeCalpico <rachelqiu0428@gmail.com>
2024-01-21 14:07:48 -0800

    commit  to fix document append undefined file issue

--- client/src/pages/Onboarding/OnboardingPage.jsx
+++ client/src/pages/Onboarding/OnboardingPage.jsx
@@ -5,7 +5,10 @@ import { useSelector, useDispatch } from "react-redux";
 import { submitOnboarding } from "../../redux/onboardingSlice";
 import { Document, Page, pdfjs } from "react-pdf";
 import FilePreviewer from "../../components/FilePreviewer";
-import { fetchUserByIdThunk, updateCurrentUserThunk } from "../../thunks/auth-thunk";
+import {
+  fetchUserByIdThunk,
+  updateCurrentUserThunk,
+} from "../../thunks/auth-thunk";
 import { updateTokenStatusThunk } from "../../thunks/token-thunk";
 import { fetchPersonalInformationByUID } from "../../redux/personalInformationSlice";
 import Header from "../layout/Header";
@@ -24,7 +27,7 @@ const OnboardingPage = () => {
   const [onboardingStatus, setOnboardingStatus] = useState(null);
   const [optReceipt, setOptReceipt] = useState();
   const u_id = localStorage.getItem("userID");
-  const {user }= useSelector((state) => state.user);
+  const { user } = useSelector((state) => state.user);
   const [newEmergencyContact, setNewEmergencyContact] = useState({
     firstName: "",
     lastName: "",
@@ -219,15 +222,16 @@ const OnboardingPage = () => {
     createInfo(e);
     dispatch(updateTokenStatusThunk(user.email));
     dispatch(updateCurrentUserThunk(user.id, { onboardingStatus: "pending" }));
-  
   };
 
   async function createInfo(e) {
     e.preventDefault();
-    console.log("Form submitted:", formData);
-    console.log("File submitted:", optReceipt);
     // TODO: MAY NOT BE ANY FILES
-    document.append("file", optReceipt, optReceipt.name);
+    if (
+      formData.citizenship === "no" &&
+      formData.workAuthorization.workAuthorizationType === "F1(CPT/OPT)"
+    )
+      document.append("file", optReceipt, optReceipt.name);
     const payload = {
       formData,
       u_id,
