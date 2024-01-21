73ebe7659c78c57313954ba97b54fae52a544e64
LycheeCalpico <rachelqiu0428@gmail.com>
2024-01-20 23:46:12 -0800

    fix: only opt status can see visapage right now

--- client/src/pages/VisaManagement/VisaPage.jsx
+++ client/src/pages/VisaManagement/VisaPage.jsx
@@ -17,6 +17,7 @@ const VisaPage = () => {
   const [optEADtStatus, setOptEADtStatus] = useState("never uploaded");
   const [I983Status, setI983Status] = useState("never uploaded");
   const [I20Status, setI20Status] = useState("never uploaded");
+  const [isOpt, setIsOpt] = useState();
   useEffect(() => {
     const fetchDocs = async () => {
       // dispatch(fetchUserByIdThunk(user.id)).then()
@@ -28,7 +29,9 @@ const VisaPage = () => {
           },
         }
       );
+      console.log(response);
       if (response.status === 200) {
+        setIsOpt(true);
         const optData = response.data;
         setInfo(response.data);
         if (optData.optReceipt) {
@@ -43,6 +46,8 @@ const VisaPage = () => {
         if (optData.I20) {
           setI20Status(optData.I20.status);
         }
+      } else if (response.status === 204) {
+        setIsOpt(false);
       }
     };
     fetchDocs();
@@ -50,49 +55,63 @@ const VisaPage = () => {
 
   return (
     <>
-      <Header />
+      {!isOpt && (
+        <div className="flex flex-col h-screen">
+          <Header />
+          <div className="flex grow justify-center items-center">
+            <p className="text-geekblue text-6xl">
+              Oops! You're not OPT status{" "}
+            </p>
+          </div>
+        </div>
+      )}
       {/* TODO: change to real name */}
-      <div className="text-4xl text-slate-400 mx-20 my-5">{`Hi, welcome to documents management`}</div>
-      <div className="text-3xl text-geekblue mx-20 mb-10">
-        Visa Management System
-      </div>
-      <div className="w-full flex flex-col items-center">
-        <FileUploader
-          title="OPT Receipt"
-          fileType="optReceipt"
-          status={optReceiptStatus}
-          feedback={info.optReceipt ? info.optReceipt.feedback : undefined}
-          next="OPT EAD"
-          prev="approved"
-        />
+      {isOpt && (
+        <>
+          <Header />
+          <div className="text-4xl text-slate-400 mx-20 my-5">{`Hi, welcome`}</div>
+          <div className="text-3xl text-geekblue mx-20 mb-10">
+            Visa Management System
+          </div>
+          <div className="w-full flex flex-col items-center">
+            <FileUploader
+              title="OPT Receipt"
+              fileType="optReceipt"
+              status={optReceiptStatus}
+              feedback={info.optReceipt ? info.optReceipt.feedback : undefined}
+              next="OPT EAD"
+              prev="approved"
+            />
 
-        <FileUploader
-          title="OPT EAD"
-          fileType="optEAD"
-          status={optEADtStatus}
-          feedback={info.optEAD ? info.optEAD.feedback : undefined}
-          next="I-983"
-          prev={optReceiptStatus}
-        />
+            <FileUploader
+              title="OPT EAD"
+              fileType="optEAD"
+              status={optEADtStatus}
+              feedback={info.optEAD ? info.optEAD.feedback : undefined}
+              next="I-983"
+              prev={optReceiptStatus}
+            />
 
-        <FileUploader
-          title="I-983"
-          fileType="I983"
-          status={I983Status}
-          feedback={info.I983 ? info.I983.feedback : undefined}
-          next="I-20"
-          prev={optEADtStatus}
-        />
+            <FileUploader
+              title="I-983"
+              fileType="I983"
+              status={I983Status}
+              feedback={info.I983 ? info.I983.feedback : undefined}
+              next="I-20"
+              prev={optEADtStatus}
+            />
 
-        <FileUploader
-          title="I-20"
-          fileType="I20"
-          status={I20Status}
-          feedback={info.I20 ? info.I20.feedback : undefined}
-          next=""
-          prev={I983Status}
-        />
-      </div>
+            <FileUploader
+              title="I-20"
+              fileType="I20"
+              status={I20Status}
+              feedback={info.I20 ? info.I20.feedback : undefined}
+              next=""
+              prev={I983Status}
+            />
+          </div>
+        </>
+      )}
     </>
   );
 };
