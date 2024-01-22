bcbdf927865c3f78e0a34e7b4a4b317c3f1f0951
LycheeCalpico <rachelqiu0428@gmail.com>
2024-01-21 21:18:24 -0800

    remove comment

--- client/src/components/RejectFeedback.jsx
+++ client/src/components/RejectFeedback.jsx
@@ -73,52 +73,6 @@ const RejectFeedback = (props) => {
       </Modal>
     </>
   );
-  // const [open, setOpen] = useState(false);
-
-  //   const showModal = () => {
-  //     setIsModalOpen(true);
-  //   };
-
-  //   const handleOk = (e) => {
-  //     console.log("hereaidjisjd");
-  //     setIsModalOpen(false);
-  //   };
-
-  //   const handleCancel = (e) => {
-  //     console.log("kekekek");
-  //     setIsModalOpen(false);
-  //   };
-
-  //   return (
-  //     <>
-  //       <Popover content="Reject And Send Feedback">
-  //         <Button
-  //           type="primary"
-  //           onClick={showModal}
-  // className="h-8 w-20"
-  // danger
-  // ghost
-  //         >
-  //           Reject
-  //         </Button>
-  //       </Popover>
-
-  //       <Modal
-  //         title="Feedback"
-  //         open={isModalOpen}
-  //         onOK={handleOk}
-  //         onCancel={handleCancel}
-  // okButtonProps={{
-  //   style: { backgroundColor: "#85a5ff", color: "black" },
-  // }}
-  // cancelButtonProps={{}}
-  //         okText="Send"
-  //         cancelText="Cancel"
-  //       >
-  //         <TextArea rows={8} placeholder="maxLength is 300" maxLength={300} />
-  //       </Modal>
-  //     </>
-  //   );
 };
 
 export default RejectFeedback;
